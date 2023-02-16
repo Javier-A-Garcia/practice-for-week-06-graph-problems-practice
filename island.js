fc0a4e13bff72f0ac66ac78e39f97ddc09826e51
Javier Garcia <javiergarcia7991@gmail.com>
2023-02-16 10:29:17 -0800

    Passing part 2

--- island.js
+++ island.js
@@ -33,30 +33,45 @@ function getNeighbors(row, col, graph) {
 
 
 function islandSize(row, col, graph) {
+  // Your code here
 
   // Create a visited set to store visited nodes
+  let visited = new Set();
 
   // Create a stack, put the starting node in the stack
+  let stack = [[row, col]];
 
   // Put the stringified starting node in visited
-
+  visited.add(stack[0].toString());
 
   // Initialize size to 0
+  let size = 0;
 
   // While the stack is not empty,
-
+  while (stack.length > 0) {
     // Pop the first node
+    let currentNode = stack.pop();
 
     // DO THE THING (increment size by 1)
+    size++;
 
     // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
+    let [row, col] = [currentNode[0], currentNode[1]];
     // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
+    let neighbors = getNeighbors(row, col, graph);
+
+    neighbors.forEach(neighbor => {
+      // HINT: Remember, you're storing your visited nodes as strings!
+      if (!visited.has(neighbor.toString())) {
+        stack.push(neighbor);
+        // and mark them as visited
+        visited.add(neighbor.toString());
+      }
+    });
+  }
 
   // return size
-
-  // Your code here
+  return size;
 }
 
 module.exports = [getNeighbors, islandSize];
